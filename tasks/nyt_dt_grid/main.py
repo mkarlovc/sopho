4efbe61e8f92b030cdf5cf0ff6fe9a4bf1b845f7
Mario <mario.karlovcec@gmail.com>
2015-09-21 19:36:40 +0200

    grid stuff matrix there

--- tasks/nyt_dt_grid/main.py
+++ tasks/nyt_dt_grid/main.py
@@ -2,6 +2,7 @@ import datetime
 import snap
 import pickle
 import os
+import time
 
 class Edge:
     
@@ -64,7 +65,7 @@ class TempGraph:
         map = {}
         nodes = 0
         for i,e in enumerate(self.edges):
-            key = str(e.delta)+'_'+str(round(e.dist,2))            
+            key = str(round(float(e.delta)/120,1))+'_'+str(round(e.dist,1))            
             if not grid.has_key(key):
                 g = snap.TNGraph.New()
                 grid[key] = g
@@ -98,22 +99,61 @@ class TempGraph:
   
         return (grid,map)
 
+t1 = time.time()
+
 tg = TempGraph("Test temporal graph")
 
-with open("/media/mario/My Book/mario/sopho/nyt/nyt/graph/nyt-06-fast/graph1M.txt", "r") as ins:
+with open("/media/mario/My Book/mario/sopho/nyt/nyt/graph/nyt-06-fast/graph.txt", "r") as ins:
     ins.readline()
     for line in ins:
         line = line.rstrip('\n')
         edg = Edge(line.split('\t')[0], line.split('\t')[1], float(line.split('\t')[2]), float(line.split('\t')[4]))
         tg.addEdge(edg)
 
+# calculate grid
 obj = tg.createGraphGrid()
 grid = obj[0]
 map = obj[1]
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# calculate dataframe
+columns = {}
+index = {}
+
+
+for g in grid:
+    columns[float(g.split('_')[0])] = 1
+    index[float(g.split('_')[1])] = 1
+
+columns = columns.keys()
+index = index.keys()
+df = pd.DataFrame(index=sorted(index), columns=sorted(columns))
+df = df.fillna(0)
+
+for g in grid:
+    col = float(g.split('_')[0])
+    ind = float(g.split('_')[1])
+    graph = grid[g]
+    Components = snap.TCnComV()
+    snap.GetWccs(graph,Components)
+    df[col][ind] = Components.Len()
+
+plt.pcolor(df)
+plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
+plt.show()
+
+# save grid
 if not os.path.exists("./grid/"):
     os.makedirs("./grid/")
 
 for g in grid:
     FOut = snap.TFOut("grid/g_"+g.replace('.',''))
     grid[g].Save(FOut)
+
+t2 = time.time()
+
+print t2-t1
