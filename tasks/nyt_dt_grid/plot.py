61942c89dd146250637741d95f391d5f0de53352
mkarlovc <mario.karlovcec@gmail.com>
2015-10-02 10:20:57 +0200

    save to csv

--- tasks/nyt_dt_grid/plot.py
+++ tasks/nyt_dt_grid/plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 
 df = pd.load(sys.argv[1])
+df.to_csv(sys.argv[1]+".txt")
 plt.pcolor(df)
 plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
 plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
